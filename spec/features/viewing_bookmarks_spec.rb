08c603cac59316a6b01b2bdc558b3d048baf66a3
Marianne Azzopardi <marianneazzopardi@Mariannes-MacBook-Air.local>
2021-10-11 14:42:47 +0100

    added bookmark class and erb file
    co-authored Charlie <https://github.com/charlie83xt>

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,6 +1,7 @@
 feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
     visit('/bookmark')
+    
 
     expect(page).to have_content "http://www.makersacademy.com"
     expect(page).to have_content "http://www.destroyallsoftware.com"
